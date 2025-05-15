26f1df4185f7f1d11719f06f6a0c9cc9f5748768
Lauren Balch <llbalch@gmail.com>
2025-05-15 09:47:43 -0500

    defined model associations

--- models/index.js
+++ models/index.js
@@ -1,8 +1,18 @@
+const { Sequelize, DataTypes, Model } = require('./db/connection.js')
 const Comment = require("./Comment");
 const Like = require("./Like");
 const Post = require("./Post");
 const Profile = require("./Profile");
 const User = require("./User");
+
+User.hasOne(Profile)
+Profile.belongsTo(User)
+User.hasMany(Post)
+Post.belongsTo(User)
+Post.hasMany(Comment)
+Comment.belongsTo(Post)
+User.belongsToMany(Like, {through: "userlike"})
+Like.belongsToMany(User, {through: "userlike"})
 
 module.exports = {
     Comment,
